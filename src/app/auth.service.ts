f53bbac893b0d9a7d92eeb65e3729b15864601ef
Caio Carvalho <caio.cfarinha14@gmail.com>
2025-06-25 16:57:01 -0300

    feat: cria service de autenticação simulado

---
+++ src/app/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly USERNAME = 'admin';
+  private readonly PASSWORD = '1234';
+
+  isAuthenticated = false;
+  
+  constructor( private router: Router) { }
+
+  login (username: string, password:string): boolean{
+    if(username === this.USERNAME && password === this.PASSWORD){
+      this.isAuthenticated = true;// Set authentication status
+      this.router.navigate(['/']);// Redirect to home page
+      return true;// Valid credentials
+    }
+    this.isAuthenticated = false;
+    this.router.navigate(['/login']);// Redirect to login page
+    return false;// Invalid credentials
+  }
+
+  logout(){
+    this.isAuthenticated = false;// Clear authentication status
+    this.router.navigate(['/login']);// Redirect to login page
+  }
+}
